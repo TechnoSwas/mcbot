4d4180b7c328b39d1ae6974e722c3b24945df106
TechnoSwas <swas.swastik@hotmail.com>
2026-05-15 11:43:24 +0000

    add bot

---
+++ index.js
@@ -0,0 +1,43 @@
+const mineflayer = require('mineflayer')
+const express = require('express')
+
+const app = express()
+app.get('/', (req, res) => res.send('Bot is running!'))
+app.listen(3000, () => console.log('Web server alive'))
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'sklsmplol.aternos.me',
+    port: 25565,
+    username: 'fandomboi',
+    auth: 'offline',
+    version: false
+  })
+
+  bot.on('spawn', () => {
+    console.log('fandomboi joined the server!')
+  })
+
+  // Anti-AFK
+  setInterval(() => {
+    bot.setControlState('jump', true)
+    setTimeout(() => bot.setControlState('jump', false), 500)
+  }, 30000)
+
+  bot.on('kicked', (reason) => {
+    console.log('Kicked:', reason)
+    setTimeout(createBot, 5000)
+  })
+
+  bot.on('error', (err) => {
+    console.log('Error:', err)
+    setTimeout(createBot, 5000)
+  })
+
+  bot.on('end', () => {
+    console.log('Disconnected, reconnecting...')
+    setTimeout(createBot, 5000)
+  })
+}
+
+createBot()
